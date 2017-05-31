0371affb647fd8e7d1c6295d8f3120fffbb8b77c
Wenjun Che <wenjun@openfin.co>
2017-05-31 12:18:49 -0400

    AUT-80: upgrade chromedriver for Runtime 7.53.x.x

--- test/config.js
+++ test/config.js
@@ -18,7 +18,7 @@ module.exports = (function () {
         remoteDriverPort: 9515,
         //remoteDriverPath: "/wd/hub",
         testTimeout: 20000,
-        expectedRuntimeVersion: "6.49.18.41"
+        expectedRuntimeVersion: "7.53.20.20"
     };
 
     config.remoteDriverUrl = "http://" + config.remoteDriverHost + ":" + config.remoteDriverPort + 
