4db142a24d55b3d2dd0ef7068b4ab15619a7f2e1
Wenjun Che <wenjun@openfin.co>
2019-10-23 21:55:02 -0400

    AUT-104: updated comments in RunOpenFin.bat

--- RunOpenFin.bat
+++ RunOpenFin.bat
@@ -2,7 +2,10 @@
 REM This script is intended to be used for launching OpenFin Runtime by ChromeDriver or node of Selenium Grid server.
 REM It parses --remote-debugging-port and --config parameters passed by ChromeDriver and passes them to OpenFinRVM.exe
 REM %openfinLocation% needs to point to location of OpenFin install directory.
-REM devtools_port should NOT be set in app manifest json file
+REM if devtools_port is set in app.json and ChromeDriver needs to communicate on that port,  set chromeOptions.debuggerAddress
+REM to be the same as devtools_port.
+REM Otherwise, devtools_port should NOT be set in app manifest json file and chromedriver dynamically assigns a port and passes
+REM it with --remote-debugging-port as a command line argument to this script
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 SETLOCAL ENABLEEXTENSIONS
